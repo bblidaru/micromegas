941b7c7ef5be1655f313b41eb576ee44e295c16c
Bogdan Blidaru <bblidaru@asus-n56v.nipne.ro>
2016-06-03 14:28:10 +0300

    graf

--- src/graf_pres.cpp
+++ src/graf_pres.cpp
@@ -59,7 +59,7 @@ int main(int argc, char * argv[])
       /* getopt_long stores the option index here. */
       int option_index = 0;
 
-      cp = getopt_long (argc, argv, "p:t:e:", long_options, &option_index);
+      cp = getopt_long (argc, argv, "p:t:e:o:", long_options, &option_index);
 
       /* Detect the end of the options. */
       if (cp == -1)
